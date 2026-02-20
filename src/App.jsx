3a49f8a5c92ccb8dd52c967068580b5cf17942ec
ThiagoJoelP <thiagojoel17@hotmail.com>
2026-02-20 19:37:19 -0300

    Add all module routes: Productos, Costos, Listas, Facturación, Stock

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
+import Productos from './pages/Productos'
+import Costos from './pages/Costos'
+import ListasPrecios from './pages/ListasPrecios'
+import Facturacion from './pages/Facturacion'
+import Stock from './pages/Stock'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Dashboard />} />
+        <Route path="productos" element={<Productos />} />
+        <Route path="costos" element={<Costos />} />
+        <Route path="listas-precios" element={<ListasPrecios />} />
+        <Route path="facturacion" element={<Facturacion />} />
+        <Route path="stock" element={<Stock />} />
       </Route>
     </Routes>
   )
