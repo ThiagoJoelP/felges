6aab252b0a64b30f1cf6bafada214ea8479d0fef
ThiagoJoelP <thiagojoel17@hotmail.com>
2026-02-20 19:24:43 -0300

    Remove Users/Settings routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
-import Users from './pages/Users'
-import Settings from './pages/Settings'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Dashboard />} />
-        <Route path="users" element={<Users />} />
-        <Route path="settings" element={<Settings />} />
       </Route>
     </Routes>
   )
